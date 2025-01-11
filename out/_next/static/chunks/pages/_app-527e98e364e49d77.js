7f4b5bfe13f73847c730d971e19a6b49aa0c04dc
ggcweb <garggcwebsite@gmail.com>
2025-01-11 11:40:29 -0500

    things

---
+++ out/_next/static/chunks/pages/_app-527e98e364e49d77.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{5355:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(4655)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(5355),n(3445))),_N_E=_.O()}]);
